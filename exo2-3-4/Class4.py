644c55d32a6c5a38fd70b11372b08be213469ac0
phanietwin <134687455+phanietwin@users.noreply.github.com>
2024-02-13 14:42:01 +0100

    Update Class4.py

--- exo2-3-4/Class4.py
+++ exo2-3-4/Class4.py
@@ -3,27 +3,30 @@ from math import sqrt, cos, sin
 from Class2 import*
 from Class3 import*
 
-
+#Classe couleur prenant en argument trois entiers pour les couleurs r, b,v
 class Couleur(object):
     def __init__(self, r, b, v):
         self.r = r
         self.b = b
         self.v = v
-
+        
+#Classe objet 3D, prenant comme attribut un vecteur centre, une couleur (de classe Couleur) et une liste de triangles
 class Objet3D(object):
     def __init__(self, centre, couleur):
         self.centre = centre
         self.couleur = couleur
         self.triangle = []
-
+#fonction pour ajouter des triangles à la liste, prenant en argument un triangle de type Triangle
     def Ajouter_triangle(self, triangle):
         self.triangle.append(triangle)
-
+        
+#fonction pour déplacer l'Objet 3d le long d'un vecteur de référence
     def déplacer(self, vectref):
         self.centre = self.centre.additionner(vectref)
         for triangle in self.triangle :
             triangle.Deplacer(vectref)
-
+            
+#Fonction pour afficher ses points :
     def Afficher(self):
 
         print("points de l'objet 3D:")
