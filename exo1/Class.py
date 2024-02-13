7b0e953cd50eb7c9213c65b9da0b4b04147defd5
phanietwin <134687455+phanietwin@users.noreply.github.com>
2024-02-13 14:45:25 +0100

    Update Class.py

--- exo1/Class.py
+++ exo1/Class.py
@@ -1,14 +1,17 @@
+
+#Classe Algèbre prenant en attribut et argument un entier n
 class Algebre(object):
     def __init__(self,n):
         self.n = n
-
+#fonction pour sommer tous les entiers jusqu'à n
     def somme(self,somme):
         for i in range (self.n+1):
             somme = i + somme
         print("la somme de nombre est : ",somme)
         return self.somme
+        
+    #Fonction pour calculer la factorielle d'un nombre    
     def factorielle(self):
-    
         fact = 1
         for i in range (self.n+1):
             if(i>1):
