9294f1bcbe79596df631085529c1128439d7ca80
phanietwin <134687455+phanietwin@users.noreply.github.com>
2024-02-13 14:38:45 +0100

    Update Class3.py

--- exo2-3-4/Class3.py
+++ exo2-3-4/Class3.py
@@ -2,24 +2,25 @@
 from math import sqrt, cos, sin
 from Class2 import*
 
-
+#classe triangle prenant en arguments des objet vecteur
 class Triangle(object):
     def __init__(self, v1, v2, v3):
         self.v1 = v1
         self.v2 = v2
         self.v3 = v3
-
+#Fonction pour tourner ce triangle selon un angle alpha
     def Tourner(self, alpha):
         self.v1 = self.v1.Tourner(alpha)
         self.v2 = self.v2.Tourner(alpha)
         self.v3 = self.v3.Tourner(alpha)
         return self
-
+#Fonction pour afficher les coordonnées de ses vecteurs/points
     def afficher(self):
         self.v1.afficher()
         self.v2.afficher()
         self.v3.afficher()
-
+        
+#Fonction pour le déplacer par rapport à un vecteur
     def Deplacer(self, vectref):
         self.v1 = self.v1.additionner(vectref)
         self.v2 = self.v2.additionner(vectref)
